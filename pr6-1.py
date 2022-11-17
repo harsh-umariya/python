373ffa820a82437937dfeeb5950e35c1980d4c07
harsh-umariya <103916165+harsh-umariya@users.noreply.github.com>
2022-11-17 10:45:45 +0530

    Add files via upload

---
+++ pr6-1.py
@@ -0,0 +1,34 @@
+print("----->Create a list.<-----")
+L1=[1,2,3,4]
+print(L1)
+
+L2=['harsh',1,'cs']
+print(L2)
+
+print("----->Add/Remove an item to/from a list.<-------")
+L1.append(11)
+print(L1)
+
+del(L1[2])
+print(L1)
+
+print("----->Revers the list.<-------")
+L1.reverse()
+print(L1)
+
+print("----->Addition of list<------")
+print(L1 + L2)
+
+print(L1 in L2)
+
+print("------>sort the list.<-----")
+List=[7,3,2,9]
+List.sort()
+print(List)
+
+print("------>Lenght of list<-----")
+print(len(List))    
+
+print("------>Insert of list.<------")
+List.insert(2,20)
+print(List)
---
+++ pr6-2.py
@@ -0,0 +1,22 @@
+num=[]
+n=int(input("Enter The Number:",num))
+for i in range (1,n+1):
+    if i>0:
+        p=p+1
+    elif i>0:
+        n1=n+1
+    else:
+        z=+1
+    a.append(i)
+
+p,n,z=0,0,0        
+print("Positive Number:")
+print("Negitive Number:")
+print("Zero:")
+
+
+
+num=[]
+n=int(input("Enter The Odd-Number:",num))
+for odd in range (num):      
+    if
