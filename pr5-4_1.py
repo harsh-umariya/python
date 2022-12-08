217c5803ad1894e165888924e3a56a937bddc3ca
harsh-umariya <103916165+harsh-umariya@users.noreply.github.com>
2022-12-08 11:38:43 +0530

    Add files via upload

---
+++ pr5-4_1.py
@@ -0,0 +1,6 @@
+for i in range(33,127):
+    print(i,"=",chr(i),end="\t")
+    if(i%5==0):
+        print("\n")
+        
+        #American Standard Code for Information Interchange
---
+++ pr5-5_1.py
@@ -0,0 +1,10 @@
+i=1
+j=3
+n=0
+
+while j<=99:
+    ans=i/j
+    i=j
+    j=j+2
+    n+=ans
+    print(n)
